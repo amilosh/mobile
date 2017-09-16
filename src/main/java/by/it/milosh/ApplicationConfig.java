0a0a81c141321f1c225204e0fdcfdcf7836d98ee
Dzmitry Misiuk <minsler.by@gmail.com>
2017-09-17 00:22:51 +0300

    Configured gradle to make deployment war

--- src/main/java/by/it/milosh/ApplicationConfig.java
+++ src/main/java/by/it/milosh/ApplicationConfig.java
@@ -2,16 +2,23 @@ package by.it.milosh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class ApplicationConfig {
+public class ApplicationConfig extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationConfig.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationConfig.class);
     }
 
 }
